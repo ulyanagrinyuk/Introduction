d8386106d6fde152c6fb4ebb8a66e1a5fc21f3b1
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2023-09-27 13:17:40 +0300

    Encoding

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,9 +1,11 @@
 ﻿//#define		LOGICAL_TYPES
 //#define		CHARACTER_TYPES
 //#define		INTEGRAL_TYPES
+//#define		DECIMAL_TYPES
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -48,7 +50,7 @@ namespace DataTypes
 			Console.WriteLine("ULONG\t" + ulong.MinValue + "..." + ulong.MaxValue);
 			Console.WriteLine("ULONG\t" + ulong.MaxValue + "..." + ulong.MinValue);
 #endif
-
+#if DECIMAL_TYPES
 			Console.WriteLine("FLOAT:\t" + sizeof(float) + "Bytes");
 			Console.WriteLine(float.MinValue + "..." + float.MaxValue);
 
@@ -57,7 +59,79 @@ namespace DataTypes
 			Console.WriteLine(double.MinValue + "..." + double.MaxValue);
 
 			Console.WriteLine("Decimal:\t" + sizeof(decimal) + "Bytes");
+#endif
 
+
+			//Console.WriteLine("Введите стоимость тетради:");
+			//double price_of_copybook = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Введите количество тетрадей:");
+			//int number_of_copybook = Convert.ToInt32(Console.ReadLine());
+			//Console.WriteLine("Введите стоимость карандаша:");
+			//double price_of_pencil = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Введите количство карадашей:");
+			//int number_of_pencil = Convert.ToInt32(Console.ReadLine());
+			//double total_price = price_of_copybook * number_of_copybook * price_of_pencil * number_of_pencil;
+			//Console.WriteLine("Общая стоимость:" + ((price_of_copybook * number_of_copybook) + (price_of_pencil * number_of_pencil)));
+
+
+			//Console.WriteLine("Введите стоимость тетради:");
+			//double price_of_copybook = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Введите количство комплектов:");
+			//double price_of_cover = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Введите количство комплектов: ");
+			//int number_of_price = Convert.ToInt32(Console.ReadLine());
+			//double total_price = price_of_copybook * price_of_cover * number_of_price;
+			//Console.WriteLine("Общая стоимость:" + ((price_of_copybook ) + (price_of_cover * number_of_price)));
+
+
+			//Console.WriteLine("Расстояние до дачи:");
+			//double distance_to_the_cottage = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Расход бензина:");
+			//double gasoline_consumption = Convert.ToDouble(Console.ReadLine());
+			//Console.WriteLine("Цена литра бензина:");
+			//int price_of_gasoline = Convert.ToInt32(Console.ReadLine());
+			//double trip_to_the_cottageand_back = distance_to_the_cottage * gasoline_consumption * price_of_gasoline;
+			//Console.WriteLine("Общая стоимость:" + trip_to_the_cottageand_back);
+
+			
+			double a;
+			double b;
+			char oper;
+			double result;
+			Console.WriteLine("Введите первое число:");
+			a = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Введите оператор:");
+			oper = Convert.ToChar(Console.ReadLine());
+
+			Console.WriteLine("Введите второе число:");
+			b = Convert.ToDouble(Console.ReadLine());
+
+			if (oper == '+')
+			{
+				result = a + b;
+				Console.WriteLine("Сумма чисел равна: " +  result);
+				Console.ReadKey();
+			}
+			else if(oper == '-')
+			{
+				result = a - b;
+				Console.WriteLine("Разность чисел равна: " + result);
+				Console.ReadKey();
+			}
+			else if (oper == '*')
+			{
+				result = a * b;
+				Console.WriteLine("Умножение чисел равна: " + result);
+				Console.ReadKey();
+
+			}
+			else if (oper == '/')
+			{
+				result = a / b;
+				Console.WriteLine("Деление чисел равна: " + result);
+				Console.ReadKey();
+			}
 		}
 	}
 }
